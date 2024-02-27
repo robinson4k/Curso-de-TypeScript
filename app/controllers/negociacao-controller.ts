8a7816ae923fecd5d637c19465054d313ac5e1dc
Robinson Silva <robinson4k@gmail.com>
2024-02-27 16:54:08 -0400

    Convertendo dados de entrada

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -15,10 +15,15 @@ export class NegociacaoController
 
     adiciona()
     {
+        const exp = /-/g
+        const date = new Date(this.inputData.value.replace(exp, ','))
+        const quantidade = parseInt(this.inputQuantidade.value)
+        const valor = parseFloat(this.inputValor.value)
+
         const negociacao = new Negociacao(
-            this.inputData.value,
-            this.inputQuantidade.value,
-            this.inputValor.value
+            date,
+            quantidade,
+            valor
         )
 
         console.log(negociacao)
