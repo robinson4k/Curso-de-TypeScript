13578c186a54b7257ceff893a57c24d98584711d
Robinson Silva <robinson4k@gmail.com>
2024-02-27 16:42:00 -0400

    O tipo implícito any

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,10 +1,10 @@
 export class Negociacao
 {
-    private _data
-    private _quantidade
-    private _valor
+    private _data: Date
+    private _quantidade: number
+    private _valor: number
 
-    constructor(data, quantidade, valor)
+    constructor(data: Date, quantidade: number, valor: number)
     {
         this._data = data
         this._quantidade = quantidade
