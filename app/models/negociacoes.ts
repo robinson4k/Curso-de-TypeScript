719eb708f2069d2b3a8e7312260b9dd19995bc87
Robinson Silva <robinson4k@gmail.com>
2024-03-07 16:12:31 -0400

    Avançando com nosso modelo

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,5 +1,16 @@
 import { Negociacao } from "./negociacao";
 
-export class Negociacoes {
+export class Negociacoes
+{
     private negociacoes: Array<Negociacao> = []
+
+    adiciona(negociacao: Negociacao)
+    {
+        this.negociacoes.push(negociacao)
+    }
+
+    lista(): Array<Negociacao>
+    {
+        return this.negociacoes
+    }
 }
