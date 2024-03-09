b27786c9decb1635a34ab004351530cfc03b4157
Robinson Silva <robinson4k@gmail.com>
2024-03-09 13:21:12 -0400

    Mais sobre readonly

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,21 +1,12 @@
 export class Negociacao
 {
     constructor(
-        private _data: Date,
-        private _quantidade: number,
-        private _valor: number
+        private readonly data: Date,
+        private readonly quantidade: number,
+        private readonly valor: number
     ) {}
 
-    get data(): Date {
-        return this._data
-    }
-    get quantidade(): number {
-        return this._quantidade
-    }
-    get valor(): number {
-        return this._valor
-    }
     get volume(): number {
-        return this._quantidade * this._valor
+        return this.quantidade * this.valor
     }
 }
