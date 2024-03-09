3dfaa6f4aafcb25d03c3a046b94f890d6663cd07
Robinson Silva <robinson4k@gmail.com>
2024-03-09 13:18:13 -0400

    Arrays e Generics

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -2,14 +2,14 @@ import { Negociacao } from "./negociacao";
 
 export class Negociacoes
 {
-    private negociacoes: Array<Negociacao> = []
+    private negociacoes: Negociacao[] = []
 
     adiciona(negociacao: Negociacao)
     {
         this.negociacoes.push(negociacao)
     }
 
-    lista(): ReadonlyArray<Negociacao>
+    lista(): readonly Negociacao[]
     {
         return this.negociacoes
     }
